ba96b210fa59c174b62f8ca8b26645afa491f84c
Andrew <lumanvr@yandex.ru>
2021-10-31 16:26:06 +0500

    Создание Drawer'a

--- Application/lib/main.dart
+++ Application/lib/main.dart
@@ -1,113 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  final appTitle = 'Drawer Demo';
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF6200EE),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _selectedDestination = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    var Name = "Андрей";
+    var Surname = "Балин";
+    var School = "63";
+    var Class = "10";
+    var ClassLit = "Б";
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            DrawerHeader(
+              margin: EdgeInsets.zero,
+              //padding: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                  color: Color(0xFF6200EE),
+                ),
+                  /*image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image:  AssetImage('path/to/header_background.png'))),*/
+              child: Stack(
+                children: <Widget>[
+                  /*Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                     padding: const EdgeInsets.all(16.0),
+                    ),
+                  ),*/
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage("https://sun9-2.userapi.com/impg/LFiXijz4xRcA9NB7jSMyT8pwZfBuJbh_iioANQ/lfotjFgYISM.jpg?size=252x244&quality=96&sign=e615a7e3618ace8f3c822379134b17e6&type=album"),
+                      radius: 60,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                        '$Name $Surname',
+                        style: TextStyle(color: Colors.white, fontSize: 18), /*textTheme.headline6,*/
+                      ),
+                    //),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight+Alignment(0, .4),
+                    child: Text(
+                      '$School | $Class"$ClassLit"',
+                      style: TextStyle(color: Colors.white, fontSize: 14), /*textTheme.headline6,*/
+                    ),
+                  )
+            ]
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+            ),
+
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            ListTile(
+              leading: Icon(Icons.import_contacts),
+              title: Text('Мои работы'),
+              selected: _selectedDestination == 0,
+              onTap: () => selectDestination(0),
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications_none),
+              title: Text('Уведомления'),
+              selected: _selectedDestination == 1,
+              onTap: () => selectDestination(1),
+            ),
+            ListTile(
+              leading: Icon(Icons.done_all),
+              title: Text('Оценки'),
+              selected: _selectedDestination == 2,
+              onTap: () => selectDestination(2),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            ListTile(
+              leading: Icon(Icons.trending_up),
+              title: Text('Прогресс'),
+              selected: _selectedDestination == 3,
+              onTap: () => selectDestination(3),
+            ),
+            ListTile(
+              leading: Icon(Icons.record_voice_over),
+              title: Text('Учителя'),
+              selected: _selectedDestination == 4,
+              onTap: () => selectDestination(4),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('Класс'),
+              selected: _selectedDestination == 5,
+              onTap: () => selectDestination(5),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer),
+              title: Text('Сообщения'),
+              selected: _selectedDestination == 6,
+              onTap: () => selectDestination(6),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            ListTile(
+              leading: Icon(Icons.feedback),
+              title: Text('Помощь'),
+              selected: _selectedDestination == 7,
+              onTap: () => selectDestination(7),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('О приложении'),
+              selected: _selectedDestination == 8,
+              onTap: () => selectDestination(8),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 32.0, 0.0, 0.0),
+              child: Text("0.0.2"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
+  }
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
   }
 }
